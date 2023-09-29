85fba78db7137ea8e02a0d191c80495fe941e596
Danilo Teixeira <danilotx.dev@gmail.com>
2023-09-29 17:39:12 -0300

    Concluindo moduo de tipos e variáves

--- tipos-variaveis/src/TiposVariaveis.java
+++ tipos-variaveis/src/TiposVariaveis.java
@@ -1,5 +1,12 @@
 public class TiposVariaveis {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello World!");
+        double salarioBase = 1808;
+        salarioBase += 100;
+
+        short numeroCurto = 1;
+        int numeroNormal = numeroCurto;
+        short numeroCurto2 = (short) numeroNormal;
+
+        System.out.println(salarioBase+" "+numeroCurto2);
     }
 }
