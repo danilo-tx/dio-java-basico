e65c8f9d9baa0341fc1c99d106a8bd4b663ad1fc
Danilo Teixeira <danilotx.dev@gmail.com>
2023-10-11 04:31:11 -0300

    testando token

--- java-anatomia-classes/src/MinhaClasse.java
+++ java-anatomia-classes/src/MinhaClasse.java
@@ -8,11 +8,8 @@ public class MinhaClasse {
         String nomeCompleto = nomeCompleto(primeiroNome, segundoNome);
 
         System.out.println(nomeCompleto);
-
     }
-
     public static String nomeCompleto (String primeiroNome, String segundoNome) {
         return "Resultado do metodo: " + primeiroNome.concat(" ").concat(segundoNome);
     }
-
 }
