ea55125f8bdc45346de7a6450421fcbdbfb69477
Danilo Teixeira <danilotx.dev@gmail.com>
2023-10-15 06:25:22 -0300

    testando opções de fluxo lógico. (If/Else || If ternário)

--- calculo-de-comissao/src/AdiantamentoDeSalario.java
+++ calculo-de-comissao/src/AdiantamentoDeSalario.java
@@ -10,19 +10,20 @@ public class AdiantamentoDeSalario {
             double meta = sc.nextDouble();
             System.out.println("Insira o valor total das vendas");
             double vendas = sc.nextDouble();
-            int premiacao = 0;
+            int premiacao = vendas >= meta * 1.2 ? 1685 : vendas >= meta * 1.1 ? 1217 : vendas >= meta ? 936 : 0;
             double adiantamento = salarioBase * 0.4;
             double comissao = vendas * 0.022;
 
-            if(vendas >= meta){
-                if(vendas >= meta * 1.1 && vendas < meta * 1.2){
+/*             if(vendas >= meta){
+                if(vendas >= meta * 1.1 && vendas < meta * 1.2)
                     premiacao = 1217;
-                }else if(vendas >= meta * 1.2){
-                        premiacao = 1685;
-                }else{
+                else if(vendas >= meta * 1.2)
+                    premiacao = 1685;
+                else
                     premiacao = 936;
-                }
-            }
+            } */
+
+
             System.out.println("Você irá receber " + new DecimalFormat("R$ #,##0.00").format(premiacao + adiantamento + comissao) + " no dia 15.");
         }
     }
